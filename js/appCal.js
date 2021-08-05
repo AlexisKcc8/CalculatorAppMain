172ea97750f7ab04f1b81b418740f198c0e15de3
AlexisKcc8 <chimalx732@gmail.com>
2021-08-05 08:53:50 -0500

    anadiendo los archivos a usar

---
+++ js/appCal.js
@@ -0,0 +1,15 @@
+
+// document.write(`ANCHO: ${screen.width}
+// 				ALTO:${screen.height}<br>
+//   `);
+
+// if (screen.width <= 375) 
+//    document.write ("Pequeña") 
+// else 
+//    if (screen.width < 790) 
+//       document.write ("Mediana") 
+//    else 
+//       document.write ("Grande")
+
+
+
